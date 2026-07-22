1949167b64fa1c27327f017952eeb3b8b784847b
Altay Taneri <taneri862@gmail.com>
2026-07-22 13:33:44 +0300

    feat(billing): add RevenueCatAuthenticationException
    New exception for RevenueCat 401/403 responses, distinct from generic RevenueCatClientException.

---
+++ src/main/java/com/saveapenny/billing/exception/RevenueCatAuthenticationException.java
@@ -0,0 +1,8 @@
+package com.saveapenny.billing.exception;
+
+public class RevenueCatAuthenticationException extends RuntimeException {
+
+    public RevenueCatAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
